2ec8ed04083d62881065bdcaa327679840ed152b
Altan <altannagji@gmail.com>
2020-05-28 01:11:05 -0500

    commented out json

--- app.py
+++ app.py
@@ -19,20 +19,20 @@ def webhook():
 
   return "ok", 200
 
-{
-  "attachments": [],
-  "avatar_url": "http://i.groupme.com/123456789",
-  "created_at": 1302623328,
-  "group_id": "1234567890",
-  "id": "1234567890",
-  "name": "John",
-  "sender_id": "12345",
-  "sender_type": "user",
-  "source_guid": "GUID",
-  "system": false,
-  "text": "Hello world ☃☃",
-  "user_id": "1234567890"
-}
+# {
+#   "attachments": [],
+#   "avatar_url": "https://i.groupme.com/750x1000.jpeg.a6bd93ffed43420493af7116ab1515c8",
+#   "created_at": 1302623328,
+#   "group_id": "59969540",
+#   "id": "1234567890",
+#   "name": "John",
+#   "sender_id": "12345",
+#   "sender_type": "user",
+#   "source_guid": "GUID",
+#   "system": False,
+#   "text": "Hello world ☃☃",
+#   "user_id": "1234567890"
+# }
 
 def send_message(msg):
     url = 'https://api.groupme.com/v3/bots/post'
